d5dd69d39c0b640ddc5d3117e1ff7bbb8811e167
jwallen30 <53883437+jwallen30@users.noreply.github.com>
2020-04-10 14:58:06 -1000

    Updated directory

--- MPCdroneLanding/dronempc/quaddyn/spring2020/A. Gibansky Code/angular_acceleration.m
+++
@@ -1,4 +0,0 @@
-function omegadot = angular_acceleration(inputs, omega, I, L, b, k)
-tau = torques(inputs, L, b, k);
-omegaddot = inv(I) * (tau - cross(omega, I * omega));
-end
--- MPCdroneLanding/dronempc/quaddyn/spring2020/A. Gibansky Code/thrust.m
+++
@@ -1,5 +0,0 @@
-function T = thrust(inputs, k)
-% Inputs are values for ?i
-
-T = [0; 0; k * sum(inputs)];
-end
